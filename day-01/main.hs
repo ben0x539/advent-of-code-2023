f0f1a758a3b1d6b033732df7522fe333def2ca63
Benjamin Herr <git@1d6.org>
2023-12-01 17:29:08 -0800

    day 1 haskell

---
+++ day-01/main.hs
@@ -0,0 +1,17 @@
+import Data.List
+import Data.Maybe
+import Data.Char
+import Control.Arrow
+
+digitWords = zip [1..] ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+digitify [] = Nothing
+digitify (x:xs) | isDigit x = Just (digitToInt x)
+digitify xs = fmap fst $ find ((`isPrefixOf` xs) . snd) digitWords
+
+main = do
+  input <- readFile "../inputs/day-01-input.txt"
+  let digits = map (mapMaybe digitify . tails) (lines input)
+  let firstAndLast = map (head &&& last) digits
+  let numbers = map (\(a, b) -> a * 10 + b) firstAndLast
+  print (sum numbers)
