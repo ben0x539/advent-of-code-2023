8deb5516b79f73856877708fb18303dfabc54a60
Benjamin Herr <git@1d6.org>
2023-12-06 10:32:14 -0800

    day 6 python
    ?

---
+++ day-06/main.py
@@ -0,0 +1,32 @@
+import os
+
+def run(input_file):
+    with open(input_file) as f:
+        lines = [s.split(":")[1] for s in f.readlines()]
+        races = zip(*(map(int, s.split()) for s in lines))
+        p = 1
+        for (e, r) in races:
+            c = 0
+            for t in range(0, e):
+                d = t * (e - t)
+                if d > r:
+                    c += 1
+            p *= c
+        print(p)
+
+        c = 0
+        (e, r) = (int(s.replace(" ", "")) for s in lines)
+        for t in range(0, e):
+            d = t * (e - t)
+            if d > r:
+                c += 1
+        print(c)
+
+today = os.path.dirname(os.path.realpath(__file__)).rpartition('/')[2]
+run(f"../inputs/sample-{today}.txt")
+run(f"../inputs/{today}-input.txt")
+
+#d = t * (e - t)
+#d > r
+#r < t * (e - t)
+
