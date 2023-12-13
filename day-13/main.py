8511acdfa8ea64e7e1c319db60b7fd495c579c0a
Benjamin Herr <git@1d6.org>
2023-12-13 08:31:25 -0800

    day 13 python be cuter with horiz/vert

--- day-13/main.py
+++ day-13/main.py
@@ -1,5 +1,26 @@
 import os
 
+class TransposedInner:
+    def __init__(self, inner: list[list], row: int):
+        self.inner = inner
+        self.row = row
+
+    def __getitem__(self, key):
+        return self.inner[key][self.row]
+
+    def __len__(self):
+        return len(self.inner)
+
+class Transposed:
+    def __init__(self, inner: list[list]):
+        self.inner = inner
+
+    def __getitem__(self, key):
+        return TransposedInner(self.inner, key)
+
+    def __len__(self):
+        return len(self.inner[0])
+
 def run(input_file):
     with open(input_file, "r", encoding="utf-8") as f:
         patterns = [pattern.split() for pattern in f.read().split("\n\n")]
@@ -9,43 +30,30 @@ def run(input_file):
     post_smudge_vertical_axes = []
     post_smudge_horizontal_axes = []
     for pattern in patterns:
-        pattern_post_smudge_vertical_axis = None
-        pattern_post_smudge_horizontal_axis = None
+        pattern_post_smudge_vertical_axes = []
+        pattern_post_smudge_horizontal_axes = []
 
-        for j in range(1, len(pattern[0])):
-            errs = 0
-            for i in range(len(pattern)):
-                row = pattern[i]
-                w = min(j, len(row)-j)
-                for x in range(w):
-                    if row[j-1-x] != row[j+x]:
-                        errs += 1
-                        if errs >= 2:
-                            break
-            if errs == 0:
-                vertical_axes.append(j)
-            elif errs == 1 and pattern_post_smudge_vertical_axis is None:
-                pattern_post_smudge_vertical_axis = j
+        for (f, a, s_a) in [(lambda x: x, vertical_axes, pattern_post_smudge_vertical_axes), (Transposed, horizontal_axes, pattern_post_smudge_horizontal_axes)]:
+            p = f(pattern)
+            for j in range(1, len(p[0])):
+                errs = 0
+                for i in range(len(p)):
+                    row = p[i]
+                    w = min(j, len(row)-j)
+                    for x in range(w):
+                        if row[j-1-x] != row[j+x]:
+                            errs += 1
+                            if errs >= 2:
+                                break
+                if errs == 0:
+                    a.append(j)
+                elif errs == 1:
+                    s_a.append(j)
 
-        for i in range(1, len(pattern)):
-            row = pattern[i]
-            h = min(i, len(pattern)-i)
-            errs = 0
-            for j in range(len(row)):
-                for x in range(h):
-                    if pattern[i-1-x][j] != pattern[i+x][j]:
-                        errs += 1
-                        if errs >= 2:
-                            break
-            if errs == 0:
-                horizontal_axes.append(i)
-            elif errs == 1 and pattern_post_smudge_horizontal_axis is None:
-                pattern_post_smudge_horizontal_axis = i
-
-        if pattern_post_smudge_vertical_axis is not None:
-            post_smudge_vertical_axes.append(pattern_post_smudge_vertical_axis)
-        elif pattern_post_smudge_horizontal_axis is not None:
-            post_smudge_horizontal_axes.append(pattern_post_smudge_horizontal_axis)
+        if len(pattern_post_smudge_vertical_axes) > 0:
+            post_smudge_vertical_axes.append(pattern_post_smudge_vertical_axes[0])
+        elif len(pattern_post_smudge_horizontal_axes) > 0:
+            post_smudge_horizontal_axes.append(pattern_post_smudge_horizontal_axes[0])
 
     print(sum(vertical_axes) + sum(horizontal_axes)*100)
     print(sum(post_smudge_vertical_axes) + sum(post_smudge_horizontal_axes)*100)
